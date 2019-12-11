4400779531cc699c3828bac49645585a70e81bb2
ximo <ximo.gil14@gmail.com>
2019-12-11 21:29:05 +0100

    add admin template

---
+++ resources/views/admin/example.blade.php
@@ -0,0 +1,23 @@
+@extends('admin')
+
+<!-- Begin Page Content -->
+<!-- <div class="container-fluid"> -->
+@section('content')
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800">Example</h1>
+    <p class="mb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse quidem possimus inventore officia, ab in omnis modi atque repudiandae voluptas deserunt expedita magni sed, at amet! Voluptate a debitis deleniti.</p>
+
+    <!-- Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Example</h6>
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat reiciendis iusto cum? Reprehenderit consequatur commodi inventore voluptatem? Tenetur, vel aperiam minus laboriosam dolore molestiae fugit iste error rerum repudiandae itaque.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus sed sapiente at rem tenetur dolorum earum, sit iusto suscipit incidunt velit eveniet illum sequi odio dicta qui inventore, repellendus molestiae.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio, esse obcaecati in incidunt officia delectus, atque aspernatur dolorum minima cum ullam qui quis consectetur quae iure error ea voluptatum eligendi?
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio distinctio quo quisquam, reiciendis quas sequi nobis voluptatum. Deleniti tempore eveniet, atque distinctio, hic ad quis accusantium error, eligendi delectus repellat!
+        </div>
+    </div>
+@endsection
+
