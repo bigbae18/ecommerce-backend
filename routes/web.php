b8cc4e468d69901897acfe88e214906be6a83292
Óscar Costa Boned <ocbibz@gmail.com>
2019-12-17 16:28:24 +0100

    corrección del .env y routes/web añadidos

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', function () {
+    return view('admin');
+});
+
+Route::get('/admin/example', function () {
+    return view('admin.example');
+});
